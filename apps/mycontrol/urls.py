47bd14e5b017a4a9514a583dd76fb6b736ddc16d
Wilfred Antonio Lemus Reyes <wilfred.lean.15@gmail.com>
2015-07-20 19:56:08 +0000

    agregando app mycontrol

--- apps/mycontrol/urls.py
+++ apps/mycontrol/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
+
+from .views import main
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mycontrol.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^', include('apps.mycontrol.urls', namespace="mycontrol_app")),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', main.as_view(), name='main'),
 )
